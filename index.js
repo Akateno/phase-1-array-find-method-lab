3f6e5289df0c224b203aa69ba68b52d98dd0c0d9
Erick Vargas <erickkvargas@gmail.com>
2022-04-06 09:35:40 -0700

    commit tree fa0700c2acc2c2ae491046194181ddfc739eeb24

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+//code your solution here
+
+
+//function superbowlWin(winner){
+    //return (winner.year === "W")}
+//record.find(superbowlWin);
+
+function superbowlWin(winner) {
+    let winningYear = winner.find((winner) => winner.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    }
+  }
+ 
+
+
+
+
+
+//function superbowlWin(winner) {
+    //for (const records of winner) {
+      //if (records.result === "W") {
+       //return record.firstName;
+     // }
+   // }
+  //}
